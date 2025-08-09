3b12165f5b0123bd962c377ecaf359b3e43d21a1
Nicolas Agostini <n.b.agostini@gmail.com>
2025-08-09 15:30:48 +0000

    Update post-container-creation script enabling update and supressing verbosity

--- .devcontainer/post-container-creation.sh
+++ .devcontainer/post-container-creation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Create virtual environment and install dependencies with dev extras
-uv venv
+
+# Docker volume mounting restricts the linking mode, this suppresses warnings
+export UV_LINK_MODE=copy
+
+if [ ! -d ".venv" ]; then
+    uv venv
+fi
+
 source .venv/bin/activate
-uv pip install --all-extras -r pyproject.toml -e .
+uv  pip install --all-extras -r pyproject.toml -e .
 
 export PATH="$PATH:/workspaces/serena/.venv/bin"
 
